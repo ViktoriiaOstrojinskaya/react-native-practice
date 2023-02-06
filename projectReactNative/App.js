221a1d7b127945baa10d0a4c7ccbe12ba6abce37
Viktoriia Ostrojinskaya <viktoriia.ostrojinskaya@gmail.com>
2023-02-06 00:06:01 -0500

    start navigation

--- projectReactNative/App.js
+++ projectReactNative/App.js
@@ -1,33 +1,40 @@
 import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 import RegistrationScreen from "./Screens/RegistrationScreen";
-// import LoginScreen from "./Screens/LoginScreen";
+import LoginScreen from "./Screens/LoginScreen";
 
-import * as Font from "expo-font";
-import { AppLoading } from "expo";
+// import * as Font from "expo-font";
+// import { AppLoading } from "expo";
 
-const loadApplication = async () => {
-  await Font.loadAsync({
-    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
-    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
-  });
-};
+// const loadApplication = async () => {
+//   await Font.loadAsync({
+//     "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+//     "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+//   });
+// };
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  const [isReady, setIsReady] = useState(false);
-  if (!isReady) {
-    return (
-      <AppLoading
-        startAsync={loadApplication}
-        onFinish={() => setIsReady(true)}
-        onError={console.warn}
-      />
-    );
-  }
+  // const [isReady, setIsReady] = useState(false);
+
+  // if (!isReady) {
+  //   return (
+  //     <AppLoading
+  //       startAsync={loadApplication}
+  //       onFinish={() => setIsReady(true)}
+  //       onError={console.warn}
+  //     />
+  //   );
+  // }
 
   return (
-    <>
-      <RegistrationScreen />
-      {/* <LoginScreen /> */}
-    </>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="login" component={LoginScreen} />
+        <Stack.Screen name="registration" component={RegistrationScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
